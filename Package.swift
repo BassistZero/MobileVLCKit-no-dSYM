d09ac2d02f45ec5b041a2486b0659d703bf24477
Bassist_Zero <bassist.zero@icloud.com>
2025-04-22 22:40:30 +0300

    Add 3.6.0

--- Package.swift
+++ Package.swift
@@ -5,18 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "MobileVLCKit",
-    platform: [
-        .iOS(13)
-    ]
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "MobileVLCKit",
-            targets: ["MobileVLCKit"]),
+            targets: ["MobileVLCKit"]
+        )
     ],
     targets: [
         .binaryTarget(
             name: "MobileVLCKit",
-            path: "Sources/MobileVLCKit.xcframework"
+            url: "https://github.com/BassistZero/MobileVLCKit-no-dSYM/releases/download/3.6.0/MobileVLCKit.zip",
+            checksum: "9059e8aaeb59c9e10a5aa830d157e7e60629095f8b9c49b6d4d7f418cf7d5616"
         )
     ]
 )
--- make_vlc.sh
+++ make_vlc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 rm -rf build/ || true
-rm *.xcframework
-rm LICENSE
+rm -rf *.xcframework/ || true
+rm LICENSE || true
 
 TAG_VERSION="3.6.0"
 IOS_URL="https://download.videolan.org/pub/cocoapods/prod/MobileVLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
@@ -18,14 +18,9 @@ IOS_LOCATION="build/MobileVLCKit-binary/MobileVLCKit.xcframework"
 #Merge into one xcframework
 xcodebuild -create-xcframework \
     -framework "$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/dSYMs/MobileVLCKit.framework.dSYM" \
     -framework "$IOS_LOCATION/ios-arm64_armv7_armv7s/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_armv7_armv7s/dSYMs/MobileVLCKit.framework.dSYM" \
     -output build/MobileVLCKit.xcframework
     
-ditto -c -k --sequesterRsrc --keepParent "build/MobileVLCKit.xcframework" "build/MobileVLCKit.xcframework.zip"
-
-# cp -f build/MobileVLCKit-binary/COPYING.txt ./LICENSE
 mv build/MobileVLCKit.xcframework Sources
 rm -rf build/
 zip -r MobileVLCKit.zip Sources/MobileVLCKit.xcframework/
